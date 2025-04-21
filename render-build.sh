54727f7eff572ae16e4c4903d8dab5b4ed29b225
Sergiy Yadrykhinsky <sv@yadrik.zp.ua>
2025-04-21 21:37:42 +0300

    fix: ручная установка зависимостей через pip install

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "🔥 Установка зависимостей..."
-pip install -r requirements.txt
+echo "🔥 Принудительная установка зависимостей..."
+pip install python-telegram-bot==20.7 Flask==2.3.3 gspread==5.12.0 google-auth==2.28.1 oauth2client==4.1.3
 pip freeze
